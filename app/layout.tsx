b1621e0b5a889649ce307414f262d08608b431fa
Vitor Capretz <capretzvitor@gmail.com>
2023-10-09 11:37:59 -0300

    typescript, lint, tailwindcss, app router

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={inter.className}>
+      <body className="bg-slate-950 text-white">{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,3 @@
+export default function IndexPage() {
+  return <h1 className="text-2xl">Hello world from TabNews clone</h1>;
+}
