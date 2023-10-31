5429a52c7523b2bd1e7f9671b9aff541c08b8c3d
Vitor Capretz <capretzvitor@gmail.com>
2023-10-31 09:28:58 -0300

    create database module

--- app/api/v1/status/route.ts
+++ app/api/v1/status/route.ts
@@ -1,3 +1,7 @@
+import { database } from "../../../../infra/database";
+
 export async function GET() {
+  const result = await database.query("SELECT NOW()");
+
   return Response.json({ success: true }, { status: 200 });
 }
---
+++ infra/database.ts
@@ -0,0 +1,22 @@
+import { Client, QueryConfig, QueryResult, QueryResultRow } from "pg";
+
+async function query<T extends QueryResultRow>(
+  queryObject: string | QueryConfig,
+): Promise<QueryResult<T>> {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: parseInt(process.env.POSTGRES_PORT || "5432"),
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+  await client.connect();
+
+  const result = await client.query<T>(queryObject);
+
+  await client.end();
+
+  return result;
+}
+
+export const database = { query };
